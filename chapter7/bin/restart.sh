3fcfd7aa25f9952e759ac43783b6bdf0e9ec3d5a
Richard Bullington-McGuire <richard@moduscreate.com>
2020-02-03 00:50:13 -0500

    WIP: Add Chapter 7 baseline files

---
+++ chapter7/bin/restart.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euo pipefail
+# Thanks https://stackoverflow.com/a/246128
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$DIR"
+docker-compose up -d
+sleep 2
+docker-compose ps
