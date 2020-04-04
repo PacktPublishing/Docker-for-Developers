44431ea694140f76a99133418034d5727bd07c85
Richard Bullington-McGuire <richard@moduscreate.com>
2020-04-04 00:20:47 -0400

    Fix shell redirection

--- chapter7/bin/ssh-dep.sh
+++ chapter7/bin/ssh-dep.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 prod="${shipit_prod_user}@${shipit_prod_host}"
-ssh -i "$keyfile" -o StrictHostKeyChecking=no $prod <EOF
+ssh -i "$keyfile" -o StrictHostKeyChecking=no $prod <<EOF
 cd Docker-for-Developers/chapter7
 git pull
 docker pull
