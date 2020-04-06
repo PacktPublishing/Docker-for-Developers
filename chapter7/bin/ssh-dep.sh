b89967a7ec7a2f1fee82363af4a14f36f993ce59
Richard Bullington-McGuire <richard@moduscreate.com>
2020-04-05 21:45:19 -0400

    add virtual host support to deployment

--- chapter7/bin/ssh-dep.sh
+++ chapter7/bin/ssh-dep.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env bash
 set -euo pipefail
+port=${port:-80}
 prod="${shipit_prod_user}@${shipit_prod_host}"
 staging="${shipit_staging_user}@${shipit_staging_host}"
-port=${port:-80}
 image=${image:-dockerfordevelopers/shipitclicker:latest}
 if [[ "$target" = "staging" ]]; then
     targetEnv="$staging"
+    targetHost="$shipit_staging_host"
 else
     targetEnv="$prod"
+    targetHost="$shipit_prod_host"
 fi
 ssh -i "$keyfile" -o StrictHostKeyChecking=no "$targetEnv" <<EOF
 set -euo pipefail
 cd Docker-for-Developers/chapter7
 git pull
 docker pull "$image"
-DOCKER_IMAGE="$image" PORT="$port" bin/restart.sh
+set -a
+VIRTUAL_HOST="$targetHost"
+DOCKER_IMAGE="$image"
+PORT="$port"
+bin/restart.sh
 EOF
