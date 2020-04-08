51ad217cfd6556a281b6ce627f34125974e3ba9d
Richard Bullington-McGuire <richard@moduscreate.com>
2020-04-08 18:04:32 -0400

    Purge examples

--- chapter6/src/server/api/controllers/games/controller.js
+++ chapter6/src/server/api/controllers/games/controller.js
@@ -5,23 +5,34 @@ export class Controller {
   async get(req, res) {
     const key = `${req.params.id}/${req.params.element}`;
     var redis = await RedisService.get(key);
-    l.debug({'msg': 'Redis GET complete', 'key': key, 'value': redis});
-    return res.json({ id : req.params.id, element: req.params.element, value: redis});
+    l.debug({ msg: 'Redis GET complete', key: key, value: redis });
+    return res.json({
+      id: req.params.id,
+      element: req.params.element,
+      value: redis,
+    });
   }
 
   async set(req, res) {
-    const key =  `${req.body.id}/${req.body.element}`;
+    const key = `${req.body.id}/${req.body.element}`;
     var redis = await RedisService.set(key, req.body.value);
-    l.debug({'msg': 'Redis SET complete', 'key': key, 'value': redis});
-    return res.json({ id : req.body.id, element: req.body.element, value: req.body.value});
+    l.debug({ msg: 'Redis SET complete', key: key, value: redis });
+    return res.json({
+      id: req.body.id,
+      element: req.body.element,
+      value: req.body.value,
+    });
   }
 
   async incrby(req, res) {
-    const key =  `${req.body.id}/${req.body.element}`;
+    const key = `${req.body.id}/${req.body.element}`;
     var redis = await RedisService.incrby(key, req.body.value);
-    l.debug({'msg': 'Redis INCRBY complete', 'key': key, 'value': redis});
-    return res.json({ id : req.params.id, element: req.params.element, value: redis});
+    l.debug({ msg: 'Redis INCRBY complete', key: key, value: redis });
+    return res.json({
+      id: req.params.id,
+      element: req.params.element,
+      value: redis,
+    });
   }
-
 }
 export default new Controller();
