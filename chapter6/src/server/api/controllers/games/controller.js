4c732c079d2e6ad509b3d4d197db11e310ecadbc
Richard Bullington-McGuire <richard@moduscreate.com>
2020-04-08 19:58:35 -0400

    Fix up API routes, v2 api, reformat files

--- chapter6/src/server/api/controllers/games/controller.js
+++ chapter6/src/server/api/controllers/games/controller.js
@@ -2,8 +2,8 @@ import RedisService from '../../services/redis.service';
 import l from '../../../common/logger';
 
 export class Controller {
-  async get(req, res) {
-    const key = `${req.params.id}/${req.params.element}`;
+  async getGame(req, res) {
+    const key = `${req.params.id}`;
     var redis = await RedisService.get(key);
     l.debug({ msg: 'Redis GET complete', key: key, value: redis });
     return res.json({
@@ -13,26 +13,64 @@ export class Controller {
     });
   }
 
-  async set(req, res) {
+  async createGame(req, res) {
+    const key = `${req.params.id}`;
+    var redis = await RedisService.get(key);
+    l.debug({ msg: 'Redis GET complete', key: key, value: redis });
+    if (redis != null) {
+      return res.status(201).json({
+        id: req.params.id,
+        element: 'game',
+        value: redis,
+      });
+    } else {
+      return res.status(404);
+    }
+  }
+
+  async getGameItem(req, res) {
+    const key = `${req.params.id}/${req.params.element}`;
+    var redis = await RedisService.get(key);
+    l.debug({ msg: 'Redis GET complete', key: key, value: redis });
+    if (redis != null) {
+      return res.json({
+        id: req.params.id,
+        element: req.params.element,
+        value: redis,
+      });
+    } else {
+      return res.status(404);
+    }
+  }
+
+  async setGameItem(req, res) {
     const key = `${req.body.id}/${req.body.element}`;
     var redis = await RedisService.set(key, req.body.value);
     l.debug({ msg: 'Redis SET complete', key: key, value: redis });
-    return res.json({
-      id: req.body.id,
-      element: req.body.element,
-      value: req.body.value,
-    });
+    if (redis != null) {
+      return res.status(201).json({
+        id: req.body.id,
+        element: req.body.element,
+        value: req.body.value,
+      });
+    } else {
+      return res.status(500);
+    }
   }
 
-  async incrby(req, res) {
+  async incrementGameItem(req, res) {
     const key = `${req.body.id}/${req.body.element}`;
     var redis = await RedisService.incrby(key, req.body.value);
     l.debug({ msg: 'Redis INCRBY complete', key: key, value: redis });
-    return res.json({
-      id: req.params.id,
-      element: req.params.element,
-      value: redis,
-    });
+    if (redis != null) {
+      return res.json({
+        id: req.params.id,
+        element: req.params.element,
+        value: redis,
+      });
+    } else {
+      return res.status(404);
+    }
   }
 }
 export default new Controller();
--- chapter6/src/server/routes.js
+++ chapter6/src/server/routes.js
@@ -1,5 +1,5 @@
 import gamesRouter from './api/controllers/games/router';
 
 export default function routes(app) {
-  app.use('/api/v1/games', gamesRouter);
+  app.use('/api/v2/games', gamesRouter);
 }
