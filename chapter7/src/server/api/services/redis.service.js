352781030d69ad541006330a8ceb486809bc1fc4
Richard Bullington-McGuire <richard@moduscreate.com>
2020-04-09 17:01:24 -0400

    Sync chapter7/src with chapter6/src

--- chapter7/src/server/api/services/redis.service.js
+++ chapter7/src/server/api/services/redis.service.js
@@ -1,22 +1,23 @@
-const redis = require("redis");
-const {promisify} = require('util');
+const redis = require('redis');
+const { promisify } = require('util');
 import l from '../../common/logger';
 
 class RedisDatabase {
- 
   constructor() {
     var redis_host = process.env.REDIS_HOST ?? 'localhost';
     var redis_port = process.env.REDIS_PORT ?? 6379;
     var redis_options = {};
-    var redis_url = `redis://${redis_host}:${redis_port}`
+    var redis_url = `redis://${redis_host}:${redis_port}`;
     this._client = redis.createClient(redis_url, redis_options);
-    l.info({ 
-        msg: "Redis connection established",
-        redis_url: redis_url
+    l.info({
+      msg: 'Redis connection established',
+      redis_url: redis_url,
     });
     this._client.getAsync = promisify(this._client.get).bind(this._client);
     this._client.setAsync = promisify(this._client.set).bind(this._client);
-    this._client.incrbyAsync = promisify(this._client.incrby).bind(this._client);
+    this._client.incrbyAsync = promisify(this._client.incrby).bind(
+      this._client
+    );
   }
 
   async set(id, value) {
