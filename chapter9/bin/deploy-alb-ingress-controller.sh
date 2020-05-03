de022402dc849e9ec64e459b684359ce18f3e393
Richard Bullington-McGuire <richard@moduscreate.com>
2020-05-02 20:03:39 -0400

    Improve idempotent properties of script
    Also fix nasty bug where it would not find OIDC_PROVIDER

--- chapter9/bin/deploy-alb-ingress-controller.sh
+++ chapter9/bin/deploy-alb-ingress-controller.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -euo pipefail
 echo "Deploy ALB Ingress Controller"
 echo " adapted from https://docs.aws.amazon.com/eks/latest/userguide/alb-ingress.html"
 
@@ -9,30 +10,36 @@ VERSION=${3:-v1.1.4}
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
 IAM_ROLE_NAME="eks-alb-ingress-controller"
 IAM_POLICY_NAME="ALBIngressControllerIAMPolicy"
+EKS_CONTEXT="arn:aws:eks:$REGION:$AWS_ACCOUNT_ID:cluster/$CLUSTER"
+SLEEP_DURATION=10
 
-kubectl config use-context "arn:aws:eks:$REGION:$AWS_ACCOUNT_ID:cluster/$CLUSTER"
+echo "Switching context to $EKS_CONTEXT"
+kubectl config use-context "$EKS_CONTEXT"
+echo "Associating IAM OIDC provider"
+set +e
 eksctl utils associate-iam-oidc-provider \
     --region "$REGION" \
     --cluster "$CLUSTER" \
     --approve
-IAM_POLICY_ARN=$(aws iam create-policy \
-    --region "$REGION" \
-    --policy-name "$IAM_POLICY_NAME" \
-    --policy-document https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/$VERSION/docs/examples/iam-policy.json \
-    --query 'Policy.Arn' \
-    --output text)
-aws iam attach-role-policy \
-    --role-name "$IAM_ROLE_NAME" \
-    --policy-arn="$IAM_POLICY_ARN"
-kubectl apply -f "https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/$VERSION/docs/examples/rbac-role.yaml"
+set -e
+echo -n "Finding OIDC provider: "
 OIDC_PROVIDER=$(aws eks describe-cluster \
     --region "$REGION" \
     --name "$CLUSTER" \
     --query "cluster.identity.oidc.issuer" \
     --output text \
     | sed -e "s/^https:\/\///")
-TMPFILE=$(mktmp)
-cat > "$TMPFILE" <<EOF
+echo $OIDC_PROVIDER
+echo "Getting IAM role"
+AWS_IAM_ROLE=$(aws iam get-role \
+    --role-name "$IAM_ROLE_NAME" \
+    --query Role.Arn \
+    --output text)
+if [[ -z "$AWS_IAM_ROLE" ]]; then
+    echo "Creating IAM Role"
+
+    TMPFILE=$(mktemp)
+    cat > "$TMPFILE" <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
@@ -51,16 +58,43 @@ cat > "$TMPFILE" <<EOF
   ]
 }
 EOF
-aws iam create-role \
+    aws iam create-role \
+        --role-name "$IAM_ROLE_NAME" \
+        --assume-role-policy-document "file://$TMPFILE" \
+        --description "EKS ALB Ingress controller"
+fi
+#shellcheck disable=SC2016
+IAM_POLICY_ARN=$(aws iam list-policies \
+    --query 'Policies[?PolicyName==`ALBIngressControllerIAMPolicy`].Arn' \
+    --output text)
+if [[ -z "$IAM_POLICY_ARN" ]]; then
+    echo "Creating IAM policy $IAM_POLICY_NAME"
+    IAM_POLICY_ARN=$(aws iam create-policy \
+        --policy-name "$IAM_POLICY_NAME" \
+        --policy-document "https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/$VERSION/docs/examples/iam-policy.json" \
+        --query 'Policy.Arn' \
+        --output text)
+fi
+echo "Attaching IAM Role Policy"
+aws iam attach-role-policy \
     --role-name "$IAM_ROLE_NAME" \
-    --assume-role-policy-document "file://$TMPFILE" \
-    --description "EKS ALB Ingress controller"
+    --policy-arn="$IAM_POLICY_ARN"
+echo "Applying RBAC role config"
+kubectl apply -f "https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/$VERSION/docs/examples/rbac-role.yaml"
+echo "Applying ingress controller config"
 kubectl apply -f "https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/$VERSION/docs/examples/alb-ingress-controller.yaml"
+echo "Annotating alb-ingress-controller service account with IAM role"
 kubectl annotate serviceaccount \
     --overwrite \
     -n kube-system \
     alb-ingress-controller \
     "eks.amazonaws.com/role-arn=arn:aws:iam::$AWS_ACCOUNT_ID:role/$IAM_ROLE_NAME"
+echo "Getting VPC ID"
+AWS_VPC_ID=$(aws eks describe-cluster \
+    --name "$CLUSTER" \
+    --region "$REGION" \
+    --query 'cluster.resourcesVpcConfig.vpcId' \
+    --output text)
 cat <<EOF
 The AWS documentation on setting up an ALB Ingress controller states:
 
@@ -76,7 +110,7 @@ A sample would be:
       - args:
         - --ingress-class=alb
         - --cluster-name=$CLUSTER
-        - --aws-vpc-id=vpc-03468a8157edca5bd
+        - --aws-vpc-id=$AWS_VPC_ID
         - --aws-region=$REGION
 
 Hit enter to run this command to edit the file:
@@ -87,7 +121,7 @@ EOF
 #shellcheck disable=SC2034
 read -r x
 kubectl edit deployment.apps/alb-ingress-controller -n kube-system
-echo "Sleeping for 5 seconds to let the ingress controller activate"
-sleep 5
+echo "Sleeping for $SLEEP_DURATION seconds to let the ingress controller activate"
+sleep $SLEEP_DURATION
 echo "Now running: kubectl get pods -n kube-system"
 kubectl get pods -n kube-system
