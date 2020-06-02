e3188bac5ac2491740eb767f1420d22a129cad85
Richard Bullington-McGuire <richard@moduscreate.com>
2020-06-01 23:05:40 -0400

    Add more distinct game creation log message

--- chapter10/src/server/api/controllers/games/controller.js
+++ chapter10/src/server/api/controllers/games/controller.js
@@ -23,7 +23,7 @@ export class Controller {
       const id = nanoid();
       const started_on = new Date().getTime();
       var redis = await RedisService.set(id, started_on);
-      l.debug({ msg: 'Redis SET complete', key: id, value: redis });
+      l.info({ msg: 'Game created in Redis', key: id, value: redis });
       return res.status(201).json({
         id: id,
         started_on: started_on,
@@ -38,7 +38,7 @@ export class Controller {
     try {
       const key = `${req.params.id}/${req.params.element}`;
       var redis = await RedisService.get(key);
-      l.debug({ msg: 'Redis GET complete', key: key, value: redis });
+      l.debug({ msg: 'Game item Redis GET complete', key: key, value: redis });
       return res.json({
         id: req.params.id,
         element: req.params.element,
@@ -59,7 +59,7 @@ export class Controller {
     try {
       const key = `${req.body.id}/${req.body.element}`;
       var redis = await RedisService.set(key, req.body.value);
-      l.debug({ msg: 'Redis SET complete', key: key, value: redis });
+      l.info({ msg: 'Game item Redis SET complete', key: key, value: redis });
       return res.status(201).json({
         id: req.body.id,
         element: req.body.element,
@@ -79,7 +79,7 @@ export class Controller {
     try {
       const key = `${req.body.id}/${req.body.element}`;
       var redis = await RedisService.incrby(key, req.body.value);
-      l.debug({ msg: 'Redis INCRBY complete', key: key, value: redis });
+      l.info({ msg: 'Game item Redis INCRBY complete', key: key, value: redis });
       return res.json({
         id: req.params.id,
         element: req.params.element,
