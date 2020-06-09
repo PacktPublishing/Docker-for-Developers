966213957b079b44a4984fff855ed489862ea0d5
Michael Schwartz <mykesx@gmail.com>
2020-06-09 08:38:52 -0700

    remove unneeded declare statements

--- chapter11/bin/k6.sh
+++ chapter11/bin/k6.sh
@@ -10,10 +10,6 @@ ${DEBUG:-false} && set -vx
 # and http://wiki.bash-hackers.org/scripting/debuggingtips
 export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
 
-# declare -i MOVES
-# declare -i USERS
-# declare -i DURATION
-
 TARGET=${1:?$0: Target URL parameter is required.}
 USERS=${USERS:-1}   # number of simultaneous virtual users (VUs) to simulate
 DURATION=${DURATION:-60}  # number of seconds
