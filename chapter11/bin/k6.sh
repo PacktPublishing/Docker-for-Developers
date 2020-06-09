d28a3c893fc4743ed8acd2e9857b6722e4f8e830
Michael Schwartz <mykesx@gmail.com>
2020-06-08 17:08:14 -0700

    added STAGES= env variable, made all ENV variables except for TARGET

--- chapter11/bin/k6.sh
+++ chapter11/bin/k6.sh
@@ -10,33 +10,42 @@ ${DEBUG:-false} && set -vx
 # and http://wiki.bash-hackers.org/scripting/debuggingtips
 export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
 
-declare -i MOVES
-declare -i USERS
-declare -i DURATION
+# declare -i MOVES
+# declare -i USERS
+# declare -i DURATION
 
 TARGET=${1:?$0: Target URL parameter is required.}
-USERS=${2:-1}   # number of simultaneous virtual users (VUs) to simulate
-DURATION=${3:-60}  # number of seconds
-MOVES=${4:-200} # number of moves/clicks to simulate per VU
+USERS=${USERS:-1}   # number of simultaneous virtual users (VUs) to simulate
+DURATION=${DURATION:-60}  # number of seconds
+MOVES=${MOVES:-200} # number of moves/clicks to simulate per VU
+K6_STAGES={$STAGES:-""}
+
+if [ "$K6_STAGES" == "" ]; then
+    TYPE="STRESS"
+else
+    TYPE="SOAK"
+fi
 
 cat <<EOF
 *** Starting k6.io
-Target         :   $TARGET
+Target         : $TARGET
+Test Type      : $TYPE
 Virtual Users  : $USERS
 Moves per Game : $MOVES
 Total duration : $DURATION seconds
 EOF
 
 docker run \
-    --rm \
-    -i \
-    --name k6 \
-    -eDEBUG="${DEBUG:-}" \
-    -eTARGET="$TARGET" \
-    -eMOVES="$MOVES" \
-    loadimpact/k6 \
-    run \
-    --vus "$USERS" \
-    --duration "$DURATION"s \
-    - \
-    < "$TEST_DIR/test.js"
+       --rm \
+       -i \
+       --name k6 \
+       -eDEBUG="${DEBUG:-}" \
+       -eTARGET="$TARGET" \
+       -eK6_STAGES="$K6_STAGES" \
+       -eMOVES="$MOVES" \
+       loadimpact/k6 \
+       run \
+       --vus "$USERS" \
+       --duration "$DURATION"s \
+       - \
+       < "$TEST_DIR/test.js"
