e3bf023e7d0e4996476d34dce0e45806daf4f352
Richard Bullington-McGuire <richard@moduscreate.com>
2020-07-05 19:01:01 -0400

    Split /metrics route, bump versions

--- chapter11/src/server/index.js
+++ chapter11/src/server/index.js
@@ -1,5 +1,8 @@
 import './common/env';
 import Server from './common/server';
 import routes from './routes';
+import metricsRoutes from './metricsRoutes';
+
+const metrics = new Server().router(metricsRoutes).listen(process.env.METRICS_PORT);
 
 export default new Server().router(routes).listen(process.env.PORT);
