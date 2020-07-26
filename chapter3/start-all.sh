348021fc347f2c2a869c0b3145528bc780d730de
Michael Schwartz <mykesx@gmail.com>
2020-07-26 13:19:32 -0700

    ready to merge

--- chapter3/start-all.sh
+++ chapter3/start-all.sh
@@ -3,5 +3,3 @@
 ./start-mosca.sh
 ./start-mongodb.sh
 ./start-redis.sh
-./subscriber/start-subscriber.sh
-./publisher/start-publisher.sh
--- chapter3/subscriber/start-subscriber.sh
+++ chapter3/subscriber/start-subscriber.sh
@@ -2,8 +2,6 @@
 
 # start-subscriber.sh
 
-export HOSTIP=`ip route get 8.8.8.8 | sed -n '/src/{s/.*src *\([^ ]*\).*/\1/p;q}'`
-
 yarn start
 
 
