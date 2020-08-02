0fc23f5d41d4094cbfe71bb4fb06ad08c5f98f83
Richard Bullington-McGuire <richard@moduscreate.com>
2020-08-02 13:00:33 -0400

    Start docker instead of restart

--- chapter7/bin/provision-docker.sh
+++ chapter7/bin/provision-docker.sh
@@ -13,6 +13,6 @@ sudo yum -q install -y \
 sudo usermod -aG docker "$USER"
 newgrp docker
 sudo systemctl enable docker
-sudo systemctl restart docker
+sudo systemctl start docker
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
