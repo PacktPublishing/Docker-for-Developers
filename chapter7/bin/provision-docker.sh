1db7fe93f2525a415b2f94af3cdf1b38fd596926
Richard Bullington-McGuire <richard@moduscreate.com>
2020-08-01 22:10:13 -0400

    Make code match book

--- chapter7/bin/provision-docker.sh
+++ chapter7/bin/provision-docker.sh
@@ -15,4 +15,4 @@ newgrp docker
 sudo systemctl enable docker
 sudo systemctl restart docker
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod 755 /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
