c5979115e7d96fbe9033ea5f47b8c55a1766066b
rpigu-i <8628078+rpigu-i@users.noreply.github.com>
2020-08-11 08:53:50 -0400

    Add placeholder bash script for stats command

---
+++ chapter15/stats-commands.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Replace with your values
+# docker stats <container id>
+# curl -sk <options> https://<ip>:<port>/<rest endpoint>  --cert <path/to/cert.pem> --key <path/to/key.pem -cacert <path/to/ca.pem>
+
